0f4fc16a79fcf4b974d3068a67ff6ab3d1b95c8c
oliverrochester <ogrochester@gmail.com>
2019-11-18 23:08:08 -0500

    table rendering based on visitTimeStamp

--- src/oliverComps/Activity/comps/table.js
+++ src/oliverComps/Activity/comps/table.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import TblRow from './tblRow';
+
+//1492873308  timestamp
+
+let exDate = new Date().getTime();
 let example ={
     "rfid": "011016A269",
     "feederID": "CLIF",
-    "visitTimestamp": 1492873308,
+    "visitTimestamp": exDate,
     "temperature": 44,
     "mass": 108,
     "bandCombo": "#a0/V",  
 }
 
-let options = { Today: 'Today',
-                Week:'This Week',
-                Month:'This Month',
-                Year:'This Year'
-                };
+let options = ['Today','This Month','This Year'];
 
 let headers = Object.keys(example);
-var date = new Date(1492873308*1000);
-console.log(date.getMonth())
 example = (()=>{
     let arr = [];
-    for(let i = 0; i < 50; i++){
+    for(let i = 0; i < 20; i++){
         arr.push(example);
     }
     return arr;
@@ -47,16 +45,73 @@ class Table extends Component {
     }
 
     buildOptions(options){
-        let width = 
-        |        
+        console.log(options)
+        let width = window.screen.width / options.length;
+        return options.map(element => {
+            return <div style={{
+                width: width,
+                display: 'inline-block',
+                backgroundColor: 'blanched-almond',
+                textAlign: 'center',
+                height: '100%'
+                
+            }}><button onClick={(e)=>{this.renderTable(element)}} style={{
+                height: '100%',
+                width: '50%',
+                fontSize: '125%',
+                backgroundColor: 'lightgray',
+                border: 'none',
+                borderRadius: '5%'
+            }}>{element}</button></div>
+        });
           
     }
 
     renderTable(content){
+        let sendData = [];
+        let nowTime = new Date();
+        let nowMonth = nowTime.getMonth();
+        let nowDay = nowTime.getDate();
+        let nowYear = nowTime.getFullYear();
+        
+        if(content === "Today"){
+            example.forEach(element => {
+                let visitDay = new Date(element.visitTimestamp);
+                let month = visitDay.getMonth();
+                let day = visitDay.getDate();
+                let year = visitDay.getFullYear();
+                if(nowMonth === month && nowDay === day && nowYear === year){
+                    sendData.push(element);
+                }
+            });
+            
+        }
+        else if(content === 'This Month'){
+            example.forEach(element => {
+                let visitDay = new Date(element.visitTimestamp);
+                let month = visitDay.getMonth();
+                let year = visitDay.getFullYear();
+                if(nowMonth === month && nowYear === year){
+                    sendData.push(element);
+                }
+            });
+        }
+        else{
+            example.forEach(element => {
+                let visitDay = new Date(element.visitTimestamp);
+                let year = visitDay.getFullYear();
+                if(nowYear === year){
+                    sendData.push(element);
+                }
+            });
+        }
+        return this.buildTable(sendData);
 
-    }
+        
+   }
 
     buildTable(content){
+        console.log('hello')
         return content.map(row=>{
             return <TblRow rowContent={row}></TblRow>
         })
